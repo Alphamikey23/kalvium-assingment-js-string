ac05419595a9d95be6d6782f674eae889e822a28
Alphamikey23 <eng22cse001@gmail.com>
2024-02-28 15:06:48 +0530

    second commit

--- script.js
+++ script.js
@@ -19,13 +19,16 @@ var myString3 = "This is a string";
 var result1 = myString3.repeat(3)
 //Challenge 7: Store the string as "I am a Kalvian" in a variable and convert the string into array and print the array in console.
 var myString3 = "I am a Kalvian";
-var result2 = array.from(myString3)
+const result2 = myString3.split()
+console.log(result2)
 // Challenge 8: Store the given string "Time and Tide wait for none" in a variable and using indexOf method find the index of "Ti" and print the same in console.
 var myString4 = "Time and Tide wait for none"
 var result3 = myString4.indexOf("Ti")
+console.log(result3)
 // Challenge 9: Store the given string "When life gives you lemons make lemonade" in a variable and find whether it includes "lemon".
 var myString5 = "When life gives you lemons make lemonade"
 var result4 = myString5.search("lemon")
+console.log(result4)
 // Challenge 10: Store the given string "You must be the change you wish to see in the world." in a variable and use slice to console log the word "change" and use substring method to console log the word "world".
 var myString6 = "You must be the change you wish to see in the world"
 console.log(myString6.substring(16,22))
